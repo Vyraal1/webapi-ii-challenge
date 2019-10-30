4091899aaa98bf4e24c2e946b2b9cc6b7b954c51
Vyraal1 <kevin.afable@gmail.com>
2019-10-30 16:38:01 -0400

    finished all get requests, working on posts

--- posts/post.router.js
+++ posts/post.router.js
@@ -2,7 +2,6 @@
 const router = require("express").Router();
 const db = require("../data/db");
 
-//setting up first test route
 router.get("/", (req, res) => {
   db.find()
     .then(posts => {
@@ -24,18 +23,42 @@ router.get("/:id", (req, res) => {
   //findById() doesn't seem to resolve to a promise? just to clarify, do all of them resolve into promises?
   db.findById(id)
     .then(post => {
-      return post.length
+      throw err;
+      post.length
         ? res.status(200).json(post)
         : res.status(404).json({
             message: "The post with the specified ID does not exist."
           });
     })
     .catch(err => {
-      console.log("Could not find post ${id} internally", err);
+      console.log(`Could not find post ${id} internally`, err);
       res
         .status(500)
         .json({ error: "The post information could not be retrieved." });
     });
 });
 
+router.get("/:id/comments", (req, res) => {
+  const postID = req.params.id;
+  // why is this some gnarly object? is this what a promise is?
+  // console.log("comments", db.findPostComments(postID));
+  db.findPostComments(postID)
+    .then(comments => {
+      // findPostsComments returns an empty array if not found
+      comments.length
+        ? res.status(200).json(comments)
+        : res.status(404).json({
+            message: "The post with the specified ID does not exist."
+          });
+    })
+    .catch(err => {
+      console.log(
+        `Internal error: could not find comments from post ${postID}`,
+        err
+      );
+      res
+        .status(500)
+        .json({ error: "The comments information could not be retrieved." });
+    });
+});
 module.exports = router;
